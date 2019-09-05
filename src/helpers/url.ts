fc324c1f20f5a10f6ae2365f5da016dcca8e90e9
yingxiaxin <yingxiaxin@hiynn.com>
2019-09-05 11:35:43 +0800

    1、能够发送请求；2、能够处理get请求的参数，将其拼接到url上

---
+++ src/helpers/url.ts
@@ -0,0 +1,75 @@
+import { isDate, isObject } from './util';
+
+function encode(val: string): string {
+    // 将URL内容进行编码，但是将以下符号转换回来不编码
+    // 带字母的部分正则加i标记不区分大小写
+    return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/ig, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/ig, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/ig, '[')
+    .replace(/%5D/ig, ']');
+}
+
+/**
+ * 构建get请求的url函数
+ * 构建的规则：
+ * 1、如果参数属性，对应的值是数组，如params:{foo: ['bar', 'baz']}，那么在url中的形式为：xxxx?foo[]=bar&foo[]=baz
+ * 2、如果参数属性，对应的值是对象，则为stringify后的字符串，并且经过编码
+ * 3、url中带有哈希值，即'#'符号，忽略url中'#'符号后面的内容
+ * 4、url中若已有参数，则params中的参数跟在后面，否则url末尾加上'?'，并且跟上参数
+ * 5、特殊的符号如'@'、':'、'$'、'['、']'、','转码回来，不显示编码后的代码，空格符用'+'号替代
+ * 6、如果参数的属性，对应的值是日期对象，则值用toISOString()转换成字符串
+ * 7、如果参数的属性，是null或者undefined，则url中不带上这个参数
+ * @param url axios请求的url地址
+ * @param params 请求的参数
+ */
+export function buildURL(url: string, params?: any): string {
+    if (!params) {
+        return url;
+    }
+
+    const parts: string[] = [];
+
+    Object.keys(params).forEach(key => {
+        const val = params[key];
+        // 如果属性的值是null，或者undefined，url中不会带上这个参数，直接跳过
+        if (val === null || typeof val === 'undefined') {
+            return;
+        }
+        let values = [];
+        // 如果属性的值是一个数组，那么url中key的后面要跟上'[]'
+        if (Array.isArray(val)) {
+            values = val;
+            key += '[]';
+        } else {
+            // 如果属性值不是数组，那么将它变成数组，方便处理
+            values = [val];
+        }
+        values.forEach((val) => {
+            if (isDate(val)) {
+                val = val.toISOString();
+            } else if (isObject(val)) {
+                val = JSON.stringify(val);
+            }
+            parts.push(`${encode(key)}=${encode(val)}`)
+        });
+    });
+
+    let serializedParams = parts.join('&');
+    // 如果上面的处理结果是空数组，那么join后是空字符串，不处理
+    // 同时如果url里有哈希值，即myUrl#hash形式，哈希后面的是页面内锚点或跳转，不需要在请求url中跟上
+    if (serializedParams) {
+        const markIndex = url.indexOf('#');
+        if (markIndex !== -1) {
+            url = url.slice(0, markIndex);
+        }
+        // 如果这个url已经带有参数，那么它本身带有'?'，后面加上'&'并拼接
+        // 否则，先加上'?'，并拼接
+        url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams;
+    }
+
+    return url;
+}
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Method = 'get' | 'GET'
+    | 'delete' | 'DELETE'
+    | 'post' | 'POST'
+    | 'head' | 'HEAD'
+    | 'options' | 'OPTIONS'
+    | 'put' | 'PUT'
+    | 'patch' | 'PATCH';
+
+export interface AxiosRequestConfig {
+    url: string;
+    method?: Method;
+    data?: any;
+    params?: any;
+}
